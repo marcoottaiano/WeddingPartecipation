f5ad566e91dfbd6c2d9a312d78a7a0e189062367
marcoottaiano <marco.ottaiano00@gmail.com>
2023-11-14 17:01:33 +0100

    update css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.scss';
 import Home from './views/Home';
+import ScrollToTop from 'react-scroll-to-top';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <ScrollToTop smooth style={{
+        borderRadius: '200px',
+        padding: '10px',
+        height: 'auto',
+        width: 'auto',
+        zIndex: 10,
+        opacity: 0.9,
+        color: 'white',
+        backgroundColor: '#6869AA'
+      }} color='white' />
       <Home />
-    </div>
+    </>
   );
 }
 
